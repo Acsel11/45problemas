f9201a5f02f2dde15820e3aacb5f917b0aaa28df
AXEL RICARDO MONCLOA MURO <a348752@uach.mx>
2023-04-19 09:57:08 -0600

    se agrega problema 12

---
+++ 12.js
@@ -0,0 +1,16 @@
+let sumaPares = 0;
+let sumaImpares = 0;
+
+
+for (let i = 1; i <= 100; i++) {
+ console.log(i);
+ if (i % 2 === 0) {
+   sumaPares += i;
+ } else {
+   sumaImpares += i;
+ }
+}
+
+
+console.log("La suma de los números pares es " + sumaPares);
+console.log("La suma de los números impares es " + sumaImpares);
