e9afadc9a11f635211257fffcdf9f24d2ff28bc0
AXEL RICARDO MONCLOA MURO <a348752@uach.mx>
2023-04-19 10:03:42 -0600

    se agrega problema 13

---
+++ 13.js
@@ -0,0 +1,12 @@
+let contador = 0;
+
+
+for (let i = 1; i <= 100; i++) {
+ if (i % 2 === 0 || i % 3 === 0) {
+   console.log(i);
+   contador++;
+ }
+}
+
+
+console.log("Hay " + contador + " números que son múltiplos de 2 o de 3 entre 1 y 100");
