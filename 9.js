6b7d45c778f8c00740ecd17608ac29ff96149fae
AXEL RICARDO MONCLOA MURO <a348752@uach.mx>
2023-04-19 09:55:03 -0600

    se agrega problema 9

---
+++ 9.js
@@ -0,0 +1,9 @@
+const numero = parseInt(prompt("ingrese un número: "));
+
+
+if (numero > 0) {
+ console.log("El número es positivo.");
+} else if (numero < 0) {
+ console.log("El número es negativo.");
+}
+
